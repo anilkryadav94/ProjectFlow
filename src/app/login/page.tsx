e0419f0e2866830c69e4a9353707e635912419c6
Anil kumar <ayadav232@gmail.com>
2025-07-24 06:47:19 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,9 +5,15 @@ import * as React from 'react';
 import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
+  const [isClient, setIsClient] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-background p-4">
-      <LoginForm />
+      {isClient ? <LoginForm /> : null}
     </div>
   );
 }
