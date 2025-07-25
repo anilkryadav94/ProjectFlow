5e8ae9ae28bf841ff1e559f63e1c8f2fbb5879f6
Anil kumar <ayadav232@gmail.com>
2025-07-25 05:04:42 +0000

    still error check console log

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,7 +9,6 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
       <textarea
         className={cn(
           'flex min-h-[80px] w-full rounded-md border-2 border-input bg-background px-3 py-2 text-xs ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          'disabled:bg-muted/50',
           className
         )}
         ref={ref}
